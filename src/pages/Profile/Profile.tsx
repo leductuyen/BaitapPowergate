40d3fa4112852db5d5595cc78512907592ca64b2
Tuyen Le <leductuyenbs15042001@gmail.com>
2023-04-28 14:32:02 +0700

    success baitap6

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { AuthContext } from '../../context/AuthContext'
 import Upload from './UploadImage/Upload'
+import './Profile.scss'
 
 const Profile = () => {
     const { userLogin, logout } = useContext(AuthContext)
@@ -8,22 +9,27 @@ const Profile = () => {
     //! State
 
     return (
-        <>
+        <div className="container">
             <Upload />
-            <div>
-                <div>Imgage</div>
-                <div>Email</div>
-                <div>{userLogin?.email}</div>
-                <div>User Name</div>
-                <div>{userLogin?.name}</div>
-                <div>Descript</div>
-                <div>State</div>
-                <div>{userLogin?.state}</div>
-                <div>Region</div>
-                <div>{userLogin?.region}</div>
-                <button onClick={logout}>Logout</button>
+
+            <div className="email">
+                <span className="title">Email : </span>
+                <span className="content">{userLogin?.email}</span>
             </div>
-        </>
+            <div className="user">
+                <span className="title">User Name :</span>
+                <span className="content">{userLogin?.name}</span>
+            </div>
+            <div className="state">
+                <span className="title">State :</span>
+                <span className="content">{userLogin?.state}</span>
+            </div>
+            <div className="region">
+                <span className="title">Region :</span>
+                <span className="content">{userLogin?.region}</span>
+            </div>
+            <button onClick={logout}>Logout</button>
+        </div>
     )
 }
 
--- src/pages/Profile/UploadImage/Upload.tsx
+++ src/pages/Profile/UploadImage/Upload.tsx
@@ -1,21 +1,21 @@
-import React, {
-    useState,
-    useRef,
+import {
     ChangeEvent,
     SyntheticEvent,
     useContext,
+    useRef,
+    useState,
 } from 'react'
 import ReactCrop, {
-    centerCrop,
-    makeAspectCrop,
     Crop,
     PixelCrop,
+    centerCrop,
+    makeAspectCrop,
 } from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
+import { AuthContext } from '../../../context/AuthContext'
+import './Upload.scss'
 import { canvasPreview } from './canvasPreview'
 import { useDebounceEffect } from './useDebounceEffect'
-import 'react-image-crop/dist/ReactCrop.css'
-import './Upload.scss'
-import { AuthContext } from '../../../context/AuthContext'
 
 const centerAspectCrop = (
     mediaWidth: number,
@@ -38,15 +38,21 @@ const centerAspectCrop = (
 }
 
 const Upload = () => {
+    //! Context
     const { userLogin } = useContext(AuthContext)
+    //! State
     const [imgSrc, setImgSrc] = useState(userLogin?.avatar)
-    console.log(imgSrc)
+    const [croppedImage, setCroppedImage] = useState<string | null>(null)
+    const [isImageEdited, setIsImageEdited] = useState(false)
+    const [isSaving, setIsSaving] = useState(false)
+    const avatarRef = useRef<HTMLInputElement>(null)
     const previewCanvasRef = useRef<HTMLCanvasElement>(null)
     const imgRef = useRef<HTMLImageElement>(null)
     const [crop, setCrop] = useState<Crop>()
     const [completedCrop, setCompletedCrop] = useState<PixelCrop>()
     const [aspect, setAspect] = useState<number | undefined>(16 / 9)
 
+    //! Function
     const onSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files.length > 0) {
             setCrop(undefined) // Makes crop preview update between images.
@@ -62,6 +68,34 @@ const Upload = () => {
         if (aspect) {
             const { width, height } = e.currentTarget
             setCrop(centerAspectCrop(width, height, aspect))
+        }
+    }
+    const handleSave = () => {
+        setIsSaving(true)
+        const canvas = previewCanvasRef.current
+        if (!canvas) return
+
+        const imgDataUrl = canvas.toDataURL('image/png')
+        setCroppedImage(imgDataUrl)
+        setImgSrc(imgDataUrl)
+        setIsImageEdited(true)
+        setIsSaving(false)
+    }
+    const resetState = () => {
+        setCrop(undefined)
+        setCompletedCrop(undefined)
+        setImgSrc(userLogin?.avatar ?? null)
+        setCroppedImage(null)
+        setIsImageEdited(false)
+        setAspect(16 / 9)
+        if (previewCanvasRef.current) {
+            const ctx = previewCanvasRef.current.getContext('2d')
+            if (ctx) {
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+            }
+        }
+        if (avatarRef.current) {
+            avatarRef.current.value = ''
         }
     }
 
@@ -85,26 +119,39 @@ const Upload = () => {
         [completedCrop]
     )
 
+    //! Return
     return (
         <div className="Upload">
             <div className="Crop-Controls">
-                <input type="file" accept="image/*" onChange={onSelectFile} />
+                <input
+                    type="file"
+                    accept="image/*"
+                    onChange={onSelectFile}
+                    ref={avatarRef}
+                    style={{ display: imgSrc ? 'none' : 'block' }}
+                />
             </div>
-            {!!imgSrc && (
-                <ReactCrop
-                    crop={crop}
-                    onChange={(_, percentCrop) => setCrop(percentCrop)}
-                    onComplete={(c) => setCompletedCrop(c)}
-                    aspect={aspect}
-                >
-                    <img
-                        ref={imgRef}
-                        alt="Crop me"
-                        src={imgSrc ? imgSrc : ''}
-                        onLoad={onImageLoad}
-                        className="my-image"
-                    />
-                </ReactCrop>
+            {isImageEdited ? (
+                // Render phần tử img nếu ảnh đã được chỉnh sửa
+                <img src={croppedImage ?? ''} alt="" />
+            ) : (
+                // Render phần tử ReactCrop nếu ảnh chưa được chỉnh sửa
+                !!imgSrc && (
+                    <ReactCrop
+                        crop={crop}
+                        onChange={(_, percentCrop) => setCrop(percentCrop)}
+                        onComplete={(c) => setCompletedCrop(c)}
+                        aspect={aspect}
+                    >
+                        <img
+                            ref={imgRef}
+                            alt="Crop me"
+                            src={imgSrc ? imgSrc : ''}
+                            onLoad={onImageLoad}
+                            className="my-image"
+                        />
+                    </ReactCrop>
+                )
             )}
             {!!completedCrop && (
                 <>
@@ -114,9 +161,21 @@ const Upload = () => {
                             style={{
                                 width: Math.round(completedCrop?.width ?? 0),
                                 height: Math.round(completedCrop?.height ?? 0),
+                                display: isImageEdited ? 'none' : 'block', // Ẩn canvas nếu ảnh đã được chỉnh sửa
                             }}
                         />
                     </div>
+                    <button onClick={handleSave} disabled={isSaving}>
+                        {isSaving ? 'Saving...' : 'Save'}
+                    </button>
+                    <button
+                        onClick={() => {
+                            resetState()
+                            avatarRef.current?.click()
+                        }}
+                    >
+                        Choose Image
+                    </button>
                 </>
             )}
         </div>
